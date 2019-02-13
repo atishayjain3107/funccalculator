62363d92fc39ab194105e75574e8df2eefe8158c
atishayjain3107 <atishayjain3107@gmail.com>
2019-02-13 20:07:29 +0530

    commit2

--- activity.cpp
+++ activity.cpp
@@ -6,11 +6,19 @@ void add(int a,int b)
 	sum=a+b;
 	cout<<sum;
 }
+void sub(int a,int b)
+{
+	int diff;
+	diff=a-b;
+	cout<<diff;
+}
 int main()
 {
 	int a=10,b=5;
 	cout<<"sum of two no's is "<<endl;
-	add();
+	add(a,b);
+	cout<<"\n difference of two no's is "<<endl;
+	sub(a,b);
 	return(0);
 }
 	
