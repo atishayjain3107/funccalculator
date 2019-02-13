17b52e75b68527c472ef1dbb8f13817a4ee1a46c
atishayjain3107 <atishayjain3107@gmail.com>
2019-02-13 20:04:14 +0530

    commit1

---
+++ activity.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void add(int a,int b)
+{
+	int sum;
+	sum=a+b;
+	cout<<sum;
+}
+int main()
+{
+	int a=10,b=5;
+	cout<<"sum of two no's is "<<endl;
+	add();
+	return(0);
+}
+	
